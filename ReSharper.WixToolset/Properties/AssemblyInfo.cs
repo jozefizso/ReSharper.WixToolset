11a44327e17dbff528e743c916151cc6ce9aa6fd
Jozef Izso <jozef.izso@gmail.com>
2016-07-03 18:36:46 +0200

    Base project file

---
+++ ReSharper.WixToolset/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ReSharper.WixToolset")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ReSharper.WixToolset")]
+[assembly: AssemblyCopyright("Copyright © 2016 Jozef Izso")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("78a669c6-5460-48f5-b4e7-7cf27712e284")]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
