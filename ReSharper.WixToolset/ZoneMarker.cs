150e73319274ff015bb892bf2ed7d5820dbd7c47
Jozef Izso <jozef.izso@gmail.com>
2016-07-05 16:09:59 +0200

    Basic ReSharper.WixToolset package definition

---
+++ ReSharper.WixToolset/ZoneMarker.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using JetBrains.Application.BuildScript.Application.Zones;
+using JetBrains.Platform.VisualStudio.SinceVs14.Shell.Zones;
+
+namespace ReSharper.WixToolset
+{
+    [ZoneMarker]
+    public class ZoneMarker : ISinceVs14Zone
+    {
+    }
+}
