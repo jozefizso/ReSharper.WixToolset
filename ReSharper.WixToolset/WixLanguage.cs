d54e3cf8dda6b023b1e628551cc917b793c22860
Jozef Izso <jozef.izso@gmail.com>
2016-07-05 20:20:59 +0200

    Wix project types for ReSharper platform

---
+++ ReSharper.WixToolset/WixLanguage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using JetBrains.ReSharper.Psi;
+using JetBrains.ReSharper.Psi.Xml;
+
+namespace ReSharper.WixToolset
+{
+    [LanguageDefinition(WixLanguageName)]
+    public class WixLanguage : XmlLanguage
+    {
+        public const string WixLanguageName = "WIXINSTALLER";
+
+        [CanBeNull]
+        public new static readonly WixLanguage Instance;
+
+        public WixLanguage()
+            : base(WixLanguageName, "WiX Installer")
+        {
+        }
+    }
+}
---
+++ ReSharper.WixToolset/WixProjectType.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JetBrains.ProjectModel;
+
+namespace ReSharper.WixToolset
+{
+    [ProjectFileTypeDefinition(WixProjectTypeName)]
+    public class WixProjectType : XmlProjectFileType
+    {
+        public const string WixProjectTypeName = "WIXPROJECT";
+        public static readonly string[] WixProjectExtensions = new string[] { ".wxs" };
+
+        public new static WixProjectType Instance;
+
+        public WixProjectType()
+            : base(WixProjectTypeName, "WiX Toolset Project", WixProjectExtensions)
+        {
+        }
+    }
+}
