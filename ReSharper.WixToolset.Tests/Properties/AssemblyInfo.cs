fa4b4ccda43e73e9e2ce21c775a10a0b7ea80db4
Jozef Izso <jozef.izso@gmail.com>
2016-07-06 18:59:14 +0200

    Create unit tests project

---
+++ ReSharper.WixToolset.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ReSharper.WixToolset.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ReSharper.WixToolset")]
+[assembly: AssemblyCopyright("Copyright © 2016 Jozef Izso")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("5fc2dbdc-78b7-4239-b77c-3783f2dc39b8")]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
