6fe5f0bb4a1473ce32255e1239d11d7ef8d93b2a
Jozef Izso <jozef.izso@gmail.com>
2017-02-04 15:41:56 +0100

    Update plugin version so ReSharper can load it

--- ReSharper.WixToolset.Tests/Properties/AssemblyInfo.cs
+++ ReSharper.WixToolset.Tests/Properties/AssemblyInfo.cs
@@ -7,14 +7,14 @@ using NUnit.Framework;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ReSharper.WixToolset")]
-[assembly: AssemblyCopyright("Copyright © 2016 Jozef Izso")]
+[assembly: AssemblyCopyright("Copyright © 2016-2017 Jozef Izso")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("5fc2dbdc-78b7-4239-b77c-3783f2dc39b8")]
 
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
 
 [assembly: RequiresSTA]
--- ReSharper.WixToolset/Properties/AssemblyInfo.cs
+++ ReSharper.WixToolset/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("ReSharper.WixToolset")]
@@ -6,12 +7,14 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ReSharper.WixToolset")]
-[assembly: AssemblyCopyright("Copyright © 2016 Jozef Izso")]
+[assembly: AssemblyCopyright("Copyright © 2016-2017 Jozef Izso")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("78a669c6-5460-48f5-b4e7-7cf27712e284")]
 
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: InternalsVisibleTo("ReSharper.WixToolset")]
